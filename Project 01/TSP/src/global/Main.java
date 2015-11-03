85b4eb546c106f595eb2cb032ed1dba932c3362c
HarllanAndrye <harllanandrye@gmail.com>
2015-11-03 17:48:05 -0300

    Add algorithm ACO

--- Project 01/TSP/src/global/Main.java
+++ Project 01/TSP/src/global/Main.java
@@ -4,12 +4,16 @@ import aStar.AStar;
 import depthFirstSearch.DepthFirstSearch;
 import greedySearch.NearestNeighbour;
 import geneticAlgorithm.TSPException;
+import antColonyOpt.AcoTsp;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Random;
 
 public class Main {
 
 	private static int[][] theMatrix;
 	private static int qtCities;
+	private static List<String> argList = new LinkedList<String>();
 
 	/**
 	 * Gera uma matriz aleatória de acordo com a quantidade de cidades passadas como parâmetro
@@ -57,27 +61,54 @@ public class Main {
 		}
 	}
 
+	public static void aco(String file){
+		//http://adibaba.github.io/ACOTSPJava/
+		//argList.add("--acs");
+		argList.add("--ants");
+		argList.add("20");
+
+		argList.add("--time");
+		argList.add("2");
+
+		argList.add("--tours");
+		argList.add("2");
+
+		argList.add("--tries");
+		argList.add("2");
+
+		argList.add("-i");
+		argList.add(file);
+		
+		AcoTsp.main(argList.toArray(new String[0]));
+	}
+	
 	public static void main(String[] args) {
-		qtCities = 262;
-		theMatrix = new int[qtCities][qtCities];
-
-		// Arquivos com as distâncias entre as cidades (TSPLIB)
-		//parser("/home/harllan/workspace/TSP/tours/brazil58.tsp");
-		//parser("/home/harllan/workspace/TSP/tours/eil101.tsp");
-		parser("/home/harllan/workspace/TSP/tours/gil262.tsp");
-		
-		// Gera uma matriz aleatória
-		//generatorMatrix(15);
-		
-		System.out.print(printMatrix());
-
-		// Algoritmos: executar separadamente
-		//AStar s = new AStar(theMatrix, 0, qtCities);
-		//DepthFirstSearch s = new DepthFirstSearch(theMatrix, 0, qtCities);
-		NearestNeighbour s = new NearestNeighbour(theMatrix, 0, qtCities);
-		
-		s.execute();
-		System.out.println(s.result);
+		int choose = 1; // Só para não comentar o código quando rodar outro algoritmo
+		if (choose == 1) {
+			aco("tours/brazil58.tsp");
+		}
+		else {
+			qtCities = 262;
+			theMatrix = new int[qtCities][qtCities];
+	
+			// Arquivos com as distâncias entre as cidades (TSPLIB)
+			//parser("/home/harllan/workspace/TSP/tours/brazil58.tsp");
+			//parser("/home/harllan/workspace/TSP/tours/eil101.tsp");
+			parser("/home/harllan/workspace/TSP/tours/gil262.tsp");
+			
+			// Gera uma matriz aleatória
+			//generatorMatrix(15);
+			
+			System.out.print(printMatrix());
+	
+			// Algoritmos: executar separadamente
+			//AStar s = new AStar(theMatrix, 0, qtCities);
+			//DepthFirstSearch s = new DepthFirstSearch(theMatrix, 0, qtCities);
+			NearestNeighbour s = new NearestNeighbour(theMatrix, 0, qtCities);
+			
+			s.execute();
+			System.out.println(s.result);
+		}
 	}
 
 }
